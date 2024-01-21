2c6d650996a9d761210399434f61cde8f1146183
unknown <martinyang29@gmail.com>
2024-01-21 12:14:40 -0500

    Added enemy files

---
+++ Assets/Scripts/Enemies/Rabbit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : BaseEnemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
